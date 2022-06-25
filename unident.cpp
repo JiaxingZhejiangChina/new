a6e2fb0ec920520443e1473df07473fbb6ab4fc3
JiaxingZhejiangChina <107751361+JiaxingZhejiangChina@users.noreply.github.com>
2022-06-25 10:44:50 +0800

    Add files via upload

--- unident.cpp
+++ unident.cpp
@@ -1,1 +1,19 @@
-﻿
+﻿#include<iostream>;
+#include<cstdio>
+#include<string>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	double a, b, r;
+	int k;
+	cin >> a >> b;
+	k = a / b;
+	r = a - b * k;
+	cout << r;
+	return 0;
+
+
+}
