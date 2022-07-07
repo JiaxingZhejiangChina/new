7e343ae464d1346b68efc9a523542395b930e79e
JiaxingZhejiangChina <107751361+JiaxingZhejiangChina@users.noreply.github.com>
2022-07-07 09:14:10 +0800

    Add files via upload

--- unident.cpp
+++ unident.cpp
@@ -1,19 +1,15 @@
-﻿#include<iostream>;
+﻿#include<iostream>
 #include<cstdio>
 #include<string>
 #include<math.h>
 
 using namespace std;
 
-int main()
-{
-	string s, s1;
+int main() {
 	int n;
 	cin >> n;
-	s = to_string(n);
-	s1 = s.substr(2, 1) + s.substr(1, 1) + s.substr(0, 1);
-	cout << s1;
-	return 0;
+	switch (n) {
 
+	}
 
 }
